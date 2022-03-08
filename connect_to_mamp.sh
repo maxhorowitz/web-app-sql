7e574398b98d32ba03a1e4ec3118d3c517fcde4c
maxhorowitz <maxhorowitz@gmail.com>
2022-03-08 17:37:53 +0100

    symlink

---
+++ connect_to_mamp.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+ln -s ~/Documents/S6/viam-prep-learning-list/web-app-sql/index.php /Applications/MAMP/htdocs
+ln -s ~/Documents/S6/viam-prep-learning-list/web-app-sql/insert_purchase.php /Applications/MAMP/htdocs
+ln -s ~/Documents/S6/viam-prep-learning-list/web-app-sql/delete.php /Applications/MAMP/htdocs
+ln -s ~/Documents/S6/viam-prep-learning-list/web-app-sql/connect.php /Applications/MAMP/htdocs
+ln -s ~/Documents/S6/viam-prep-learning-list/web-app-sql/spending.css /Applications/MAMP/htdocs
+ln -s ~/Documents/S6/viam-prep-learning-list/web-app-sql/README.md /Applications/MAMP/htdocs
